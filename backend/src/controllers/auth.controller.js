9a940fdbf4be134d8f9f2df988cc463e0366a482
Soumaditya-Kashyap <infinitysoumaditya@gmail.com>
2025-05-19 19:33:22 +0530

    Auth Setup Initiated

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+export const signup= (req, res) => {
+    res.send("signup route");
+};
+export const login= (req, res) => {
+    res.send("login route");
+};
+export const logout= (req, res) => {
+    res.send("logout route");
+};
