0b4d2e48eae8f3b573db53d011cb5acb152fb4db
Soumaditya-Kashyap <infinitysoumaditya@gmail.com>
2025-05-22 06:34:47 +0530

    heading towards writing the backend logic of signup page at useAuthStore,jsx

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -22,7 +22,7 @@ export const useAuthStore = create((set) => ({
     }
   },
 
-    signup: async (data) => {
+  signup: async (data) => {
     set({ isSigningUp: true });
     try {
       const res = await axiosInstance.post("/auth/signup", data);
