c66690b6f306e2fc80ab82c39869271f9680b29c
Soumaditya-Kashyap <infinitysoumaditya@gmail.com>
2025-05-27 00:19:43 +0530

    realtiemme chat integration functionality will be added

--- backend/src/controllers/message.controler.js
+++ backend/src/controllers/message.controler.js
@@ -61,7 +61,6 @@ export const sendMessage = async (req, res) => {
         //Realtime Functionality goes here => socket.io
 
         res.status(200).json(newMessage);
-        
     } catch (error) {
         console.log("Error in sendMessage controller: ", error.message);
         res.status(500).json({ error: "Internal server error" });
